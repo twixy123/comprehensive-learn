f3e245caaf252dfb970d3341572fd57061c11b10
ruslan.guzhev <ruslan.guzhev@tages.ru>
2023-12-18 09:53:53 +0500

    Added grpc server

---
+++ be/grpc-main-host/src/grpc/pb/Customer.ts
@@ -0,0 +1,18 @@
+// Original file: src/grpc/proto/tech-market.proto
+
+
+export interface Customer {
+  'uuid'?: (string);
+  'email'?: (string);
+  'name'?: (string);
+  'surname'?: (string);
+  'age'?: (number);
+}
+
+export interface Customer__Output {
+  'uuid': (string);
+  'email': (string);
+  'name': (string);
+  'surname': (string);
+  'age': (number);
+}
